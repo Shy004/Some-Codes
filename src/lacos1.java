0d363bf020a5b9c72b417e2fc82769db1eedf9d7
Aurora <aurorashy04@outlook.com>
2022-11-30 11:37:47 -0300

    Alguns exercícios

---
+++ src/lacos1.java
@@ -0,0 +1,10 @@
+public class lacos1 {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero <= 5) {
+            System.out.println(numero);
+            numero = numero + 2;
+        }
+    }
+}
