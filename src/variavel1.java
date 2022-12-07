53d294b5be115ac549bf8ca6c0391a38742cc741
Aurora <aurorashy04@outlook.com>
2022-12-07 12:10:21 -0300

    Alguns exercícios

--- src/variavel1.java
+++ src/variavel1.java
@@ -1,12 +1,70 @@
+import java.util.Scanner;
+
 public class variavel1 {
     public static void main(String[] args) {
 
+        System.out.println("Para saber a idade...");
+
         //Inserindo idade
-        int dataAtual = 2022;
-        int dataNasc = 1990;
-        int idade = dataAtual - dataNasc;
-        System.out.println("O ano de nascimento é " + dataNasc);
-        System.out.println("E o ano atual é " + dataAtual);
-        System.out.println("A idade calculada é " + idade);
+        int diaAtual = actualDay();
+        int mesAtual = actualMonth();
+        int anoAtual = actualYear();
+        int diaNasc = birthdayDay();
+        int mesNasc = birthdayMonth();
+        int anoNasc = birthdayYear();
+        int calculoAnos = (anoAtual - anoNasc);
+        int calculoMeses = (mesAtual - mesNasc);
+        int calculoDias = (diaAtual - diaNasc);
+
+        if (calculoDias < 0){
+            calculoDias = calculoDias * (-1);
+        }
+        if (calculoMeses < 0){
+            calculoMeses = calculoMeses * (-1);
+        }
+
+        String anos = calculoAnos + " anos, ";
+        String meses = calculoMeses + " meses, ";
+        String dias = calculoDias + " dias";
+        String idadeBase = "A idade do indivíduo tem " + anos + meses + dias;
+        String idade = idadeBase + " de idade.";
+
+        System.out.println(idade);
+    }
+
+    //Função inserção da data Atual
+    public static int actualDay(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira o dia atual: ");
+        return scanner.nextInt();
+    }
+
+    public static int actualMonth() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira o mês atual: ");
+        return scanner.nextInt();
+    }
+
+    public static int actualYear() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira o ano atual: ");
+        return scanner.nextInt();
+    }
+
+    //Função inserção da data de Nascimento
+    public static int birthdayDay(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira o dia de nascimento: ");
+        return scanner.nextInt();
+    }
+    public static int birthdayMonth(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira o mês de nascimento: ");
+        return scanner.nextInt();
+    }
+    public static int birthdayYear(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira o ano de nascimento: ");
+        return scanner.nextInt();
     }
 }
